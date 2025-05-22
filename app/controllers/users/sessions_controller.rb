5640fc2ca3830605ac13c9e2d247937c44550723
root <root@MSI.>
2025-05-22 12:34:33 +0900

    github lintチェック部分修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [ :create ]
 
   # GET /resource/sign_in
   # def new
@@ -20,7 +20,7 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [ :attribute ])
   end
 
   def after_sign_in_path_for(resource)
