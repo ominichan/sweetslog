c2e678567f70d0e8d422f1545cecbe18004b5193
root <root@MSI.>
2025-05-27 16:36:19 +0900

    post_createアクション追加、作成機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,20 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: "ポストが作成されました"
+    else
+      flash.now[:danger] = "エラーが起きました。入力内容を確認してください"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
