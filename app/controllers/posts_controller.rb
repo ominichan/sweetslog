9179bce46cf0789906eeb1baaf5aa0bcd52ed0c4
root <root@MSI.>
2025-05-27 16:43:22 +0900

    add:post_editアクション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
   private
 
   def post_params
