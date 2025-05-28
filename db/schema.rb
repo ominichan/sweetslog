381b1c03bfcf178dc74dd0feb0a650e1398a3255
ominichan <miyoshi344322@gmail.com>
2025-05-28 16:12:46 +0900

    add:importmap,turbo

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_28_061312) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
