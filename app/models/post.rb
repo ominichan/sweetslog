eb11858a74b96875f45be473e3e016239ab8559a
root <root@MSI.localdomain>
2025-05-28 15:16:56 +0900

    add:activestrageインストール,

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
   validates :body, presence: true, length: { maximum: 65_535 }
 
+  has_many_attached :images
+
   belongs_to :user
 end
