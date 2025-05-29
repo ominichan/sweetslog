7bac9fc27e6b89a6a9c43cd4817ec733347fb36c
ominichan <miyoshi344322@gmail.com>
2025-05-29 14:38:03 +0900

    test部分修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import $ from "jquery"
+import jquery from "jquery"
 
-window.$ = $;
+window.$ = jquery;
 window.jQuery = $;
 
 document.addEventListener("turbo:load", function() {
