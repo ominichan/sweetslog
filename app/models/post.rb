0b577bc5d8708a3a042878ff57fabe557113325a
ominichan <miyoshi344322@gmail.com>
2025-06-06 15:16:42 +0900

    add:user,postモデル記述追加、ルーティング

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,5 @@ class Post < ApplicationRecord
   has_many_attached :images
 
   belongs_to :user
+  has_many :likes, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_posts, through: :likes, source: :post
 end
