b1fc29eefeaee47179af01af4e346d9f273ead1b
ominichan <miyoshi344322@gmail.com>
2025-06-06 17:09:04 +0900

    add:お気に入りAjax化、edit:ボタン大きさ変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,11 @@
 class LikesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    current_user.like(post)
-    redirect_to posts_path, notice: "いいねしました"
+    @post = Post.find(params[:post_id])
+    current_user.like(@post)
   end
 
   def destroy
-    post = current_user.likes.find(params[:id]).post
-    current_user.unlike(post)
-    redirect_to posts_path, notice: "いいねを解除しました", status: :see_other
+    @post = current_user.likes.find(params[:id]).post
+    current_user.unlike(@post)
   end
 end
