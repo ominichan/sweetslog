00273b15c50aa3b512a0a9db65cdfa57c0530eac
ominichan <miyoshi344322@gmail.com>
2025-06-10 16:03:19 +0900

    edit:自分の投稿の配置変更、検索フォーム、ページネーション追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order(created_at: :desc)
+    @q = @user.posts.ransack(params[:q])
+    @pagy, @posts = pagy(@q.result(distinct: true).order(created_at: :desc), limit: 12)
   end
 end
