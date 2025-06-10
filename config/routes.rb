96154441c443acaf01019e1329dd84dac23bce09
ominichan <miyoshi344322@gmail.com>
2025-06-10 16:32:51 +0900

    add:お気に入り、プロフィール、個人情報変更各ボタン

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [ :show ] do
-    collection do
+    member do
       get :my_posts
     end
   end
