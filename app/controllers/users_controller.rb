8f40a0bb6902a372937397c2b66a4cfb47d24ec1
ominichan <miyoshi344322@gmail.com>
2025-06-10 16:09:57 +0900

    add:my_postページに切り出し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
+
   def show
+    @user = current_user
+  end
+
+  def my_posts
     @user = User.find(params[:id])
     @q = @user.posts.ransack(params[:q])
     @pagy, @posts = pagy(@q.result(distinct: true).order(created_at: :desc), limit: 12)
