2b5239c1b4590886a17be897bfc7dc31dacacd9b
ominichan <miyoshi344322@gmail.com>
2025-06-12 17:00:34 +0900

    lintチェック修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-  scope :popular, -> { where(id: Post_tag.group(:tag_id).order('count_tag_id desc').limit(5).count(:tag_id).keys) }
+  scope :popular, -> { where(id: Post_tag.group(:tag_id).order("count_tag_id desc").limit(5).count(:tag_id).keys) }
 
   def self.ransackable_attributes(auth_object = nil)
     [ "name" ]
