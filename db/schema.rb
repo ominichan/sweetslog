35d754a1f2ece55805281e14e0651f1d5291d1b5
ominichan <miyoshi344322@gmail.com>
2025-06-12 16:15:13 +0900

    add:タグ付け機能

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_12_060459) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
