f7e44a73aa077042d12d3c9d8b990494f9ef8e7e
ominichan <miyoshi344322@gmail.com>
2025-06-13 16:18:01 +0900

    fix:scan_ruby部分修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,19 @@ class UsersController < ApplicationController
   def my_posts
     @user = User.find(params[:id])
     @q = @user.posts.ransack(params[:q])
-    @pagy, @posts = pagy(@q.result(distinct: true).order(created_at: :desc), limit: 12)
 
     if params[:date].present?
-      date = Date.parse(params[:date]) rescue nil
-      if date
-        @posts = @user.posts.where(created_at: date.beginning_of_day..date.end_of_day).order(created_at: :asc)
+      begin
+        date = Date.parse(params[:date])
+        scoped_posts = @user.posts.where(created_at: date.beginning_of_day..date.end_of_day)
+      rescue ArgumentError
+        scoped_posts = @q.result(distinct: true)
       end
+    else
+      scoped_posts = @q.result(distinct: true)
     end
-    @pagy, @calendar_posts = pagy(@q.result(distinct: true).order(created_at: :desc), limit: 12)
+
+    @pagy, @posts = pagy(scoped_posts.order(created_at: :desc), items: 12)
+    @calendar_posts = @q.result(distinct: true) # カレンダー用（全件）
   end
 end
