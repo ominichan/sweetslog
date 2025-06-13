de7b20366b5643d9d1ea6fe2e2ee23576bb45e43
ominichan <miyoshi344322@gmail.com>
2025-06-13 13:35:33 +0900

    add:投稿件数表示カレンダー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @q = @user.posts.ransack(params[:q])
     @pagy, @posts = pagy(@q.result(distinct: true).order(created_at: :desc), limit: 12)
+
+    if params[:date].present?
+      date = Date.parse(params[:date]) rescue nil
+      if date
+        @posts = @user.posts.where(created_at: date.beginning_of_day..date.end_of_day).order(created_at: :asc)
+      end
+    end
+    @pagy, @calendar_posts = pagy(@q.result(distinct: true).order(created_at: :desc), limit: 12)
   end
 end
