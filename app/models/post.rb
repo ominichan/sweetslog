dc1ffc52a7f882851037730d60a0670e56a58157
ominichan <miyoshi344322@gmail.com>
2025-06-13 12:40:19 +0900

    add:my_postページのみ日付検索

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,7 +11,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   def self.ransackable_attributes(auth_object = nil)
-    %w[title body]
+    %w[title body created_at]
   end
 
   def self.ransackable_associations(auth_object = nil)
