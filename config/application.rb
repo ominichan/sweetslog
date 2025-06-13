73406f15f73429a955901413f0cb03b9df0d14f8
ominichan <miyoshi344322@gmail.com>
2025-06-13 13:59:26 +0900

    edit:カレンダー週開始曜日を日曜に変更

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Myapp
     # config.eager_load_paths << Rails.root.join("extras")
     config.i18n.default_locale = :ja
     config.time_zone = "Tokyo"
+    config.beginning_of_week = :sunday
   end
 end
