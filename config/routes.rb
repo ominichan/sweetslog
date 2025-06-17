4ab431d651e59bd61af2a747eebd13cc3ac261bf
ominichan <miyoshi344322@gmail.com>
2025-06-17 16:25:57 +0900

    ルーティング記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -16,11 +16,8 @@ Rails.application.routes.draw do
     end
   end
   resources :likes, only: %i[create destroy]
-  resource :diagnoses, only: [ :new ] do
-    collection do
-      get :result
-    end
-  end
+  get  "diagnosis", to: "diagnoses#new"
+  post "diagnosis/result", to: "diagnoses#result"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
