68bc58c226e384b0bb1463692bbc6f9f87b1253f
ominichan <miyoshi344322@gmail.com>
2025-07-03 17:44:57 +0900

    コメント機能非同期化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to post_path(comment.post)
-    else
-      redirect_to post_path(comment.post)
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
 
   private
