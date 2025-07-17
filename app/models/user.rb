3f795d49e4ba8d66031d3b7d442fa9609fa8e2a4
ominichan <miyoshi344322@gmail.com>
2025-07-17 16:14:17 +0900

    edit:メールアドレス正規表現

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: %i[google_oauth2]
   validates :name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "は有効なメールアドレス形式で入力してください" }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :uid, presence: true, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   has_one_attached :image
