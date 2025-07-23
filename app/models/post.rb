75d51e3dfd59485b27cb908ba4f9080693ad9c19
ominichan <miyoshi344322@gmail.com>
2025-07-23 14:13:31 +0900

    add:userモデルテスト（バリデーションチェック）

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: { maximum: 100 }
-  validates :body, presence: true, length: { maximum: 65_535 }
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, presence: true, length: { maximum: 1000 }
   validates :address, presence: true
   validates :spot, presence: true
 
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,25 +11,37 @@ RSpec.describe Post, type: :model do
     it "メニュー名(title)が無い場合にバリデーションが機能してinvalidになるか" do
       post_without_title = build(:post, title: "")
       expect(post_without_title).to be_invalid
-      expect(post_without_title.errors.full_messages_for(:title)).to eq ["メニュー名を入力してください"]
+      expect(post_without_title.errors[:title]).to eq ["を入力してください。"]
     end
 
     it "本文(body)が無い場合にバリデーションが機能してinvalidになるか" do
       post_without_body = build(:post, body: "")
       expect(post_without_body).to be_invalid
-      expect(post_without_body.errors.full_messages_for(:body)).to eq ["本文を入力してください"]
+      expect(post_without_body.errors[:body]).to eq ["を入力してください。"]
     end
 
     it "店名(spot)が無い場合にバリデーションが機能してinvalidになるか" do
       post_without_spot = build(:post, spot: "")
       expect(post_without_spot).to be_invalid
-      expect(post_without_spot.errors.full_messages_for(:spot)).to eq ["店名を入力してください"]
+      expect(post_without_spot.errors[:spot]).to eq ["を入力してください。"]
     end
 
     it "住所(address)が無い場合にバリデーションが機能してinvalidになるか" do
       post_without_address = build(:post, address: "")
       expect(post_without_address).to be_invalid
-      expect(post_without_address.errors.full_messages_for(:address)).to eq ["住所を入力してください"]
+      expect(post_without_address.errors[:address]).to eq ["を入力してください。"]
+    end
+
+    it "メニュー名(title)が30文字より多い文字数の場合にバリデーションが機能してinvalidになるか" do
+      post_too_many_characters_title = build(:post, title: "あ" * 31)
+      expect(post_too_many_characters_title).to be_invalid
+      expect(post_too_many_characters_title.errors[:title]).to eq ["は30文字以下で入力して下さい。"]
+    end
+
+    it "本文(body)が1000文字より多い文字数の場合にバリデーションが機能してinvalidになるか" do
+      post_too_many_characters_body = build(:post, body: "あ" * 1001)
+      expect(post_too_many_characters_body).to be_invalid
+      expect(post_too_many_characters_body.errors[:body]).to eq ["は1000文字以下で入力して下さい。"]
     end
   end
 end
