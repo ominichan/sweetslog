b15167b888b0a7199fc90bd0e6df51fa4e4fa7cb
ominichan <miyoshi344322@gmail.com>
2025-07-23 12:52:26 +0900

    add:postモデルテスト（バリデーションチェック）

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,5 +7,29 @@ RSpec.describe Post, type: :model do
       expect(post).to be_valid
       expect(post.errors).to be_empty
     end
+
+    it "メニュー名(title)が無い場合にバリデーションが機能してinvalidになるか" do
+      post_without_title = build(:post, title: "")
+      expect(post_without_title).to be_invalid
+      expect(post_without_title.errors.full_messages_for(:title)).to eq ["メニュー名を入力してください"]
+    end
+
+    it "本文(body)が無い場合にバリデーションが機能してinvalidになるか" do
+      post_without_body = build(:post, body: "")
+      expect(post_without_body).to be_invalid
+      expect(post_without_body.errors.full_messages_for(:body)).to eq ["本文を入力してください"]
+    end
+
+    it "店名(spot)が無い場合にバリデーションが機能してinvalidになるか" do
+      post_without_spot = build(:post, spot: "")
+      expect(post_without_spot).to be_invalid
+      expect(post_without_spot.errors.full_messages_for(:spot)).to eq ["店名を入力してください"]
+    end
+
+    it "住所(address)が無い場合にバリデーションが機能してinvalidになるか" do
+      post_without_address = build(:post, address: "")
+      expect(post_without_address).to be_invalid
+      expect(post_without_address.errors.full_messages_for(:address)).to eq ["住所を入力してください"]
+    end
   end
 end
