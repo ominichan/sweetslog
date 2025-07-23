748563cb849b1af7a332de4e0b52c7ad18781367
ominichan <miyoshi344322@gmail.com>
2025-07-23 14:16:39 +0900

    lintチェック修正

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :post do
     sequence(:title) { |n| "title#{n}" }
-    body { "テスト投稿です。"}
+    body { "テスト投稿です。" }
     spot { "大阪城" }
     address { "日本、〒540-0002 大阪府大阪市中央区大阪城１−１" }
     association :user
